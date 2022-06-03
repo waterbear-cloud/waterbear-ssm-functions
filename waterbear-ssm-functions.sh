a9a1073493b9bd0a4b63ebd02d5ae76b8b0b01ac
Kevin Lindsay <klindsay@waterbear.cloud>
2022-06-02 21:33:07 -0700

    Refactored to automate most of the user script away.

--- waterbear-ssm-functions.sh
+++ waterbear-ssm-functions.sh
@@ -1,15 +1,75 @@
-# Script Arguments
-ENVIRONMENT_ARG=$1
-SERVER_ARG=$2
-INSTANCE_ID_ARG=$3
+# SSM Functions
+#
+# ------------------------------------------
+# # User Script Example
+#
+# # Configuration
+# NETENV_NAME=<netenv name>
+# APPLICATION_NAME=<app name>
+# SSH_USERNAME=ec2-user
+# SSH_PRIVATE_KEY=~/.ssh/id_rsa
+#
+# # Load the Functions
+# . /path/to/waterbear-ssm-functions/waterbear-ssm-functions.sh
+# 
+# declare_environment prod
+# declare_environment staging
+# 
+# declare_asg <group name> <resource name>
+# 
+# ssm_command $@
+# ------------------------------------------
 
-declare -a ENVIRONMENT_ALIAS=()
+
+# Args
+ENVIRONMENT_ARG=$1
+shift
+ASG_ARG=$1
+shift
+SERVER_ARG=$1
+shift
+
+# Args: Command and Instance IP
+declare -a ENVIRONMENT_ALIAS
 declare -a ENVIRONMENT_LIST
-declare -a SERVER_LIST
+declare -a ASG_LIST
+
+
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+INSTANCE_IP_ARG=""
+if valid_ip "$1"; then    
+    INSTANCE_IP_ARG="$1"
+    shift
+fi
+
+PORT_FILE_FOLDER=~/.paco-ssh
+PORT_FILE="${PORT_FILE_FOLDER}/ssh-local-ports"
+if [ "$INSTANCE_IP_ARG" != "" ] ; then
+    INSTANCE_IP_CACHE="-${INSTANCE_IP_ARG}"
+fi
+PORT_CACHE_FILE="${PORT_FILE_FOLDER}/${NETENV_NAME}-${ENVIRONMENT_ARG}-${ASG_ARG}-${SERVER_ARG}${INSTANCE_IP_CACHE}.cache"
+PORT_PID_CACHE_FILE="${PORT_CACHE_FILE}.port-pid"
+
 
 function usage()
 {
-    echo "usage: $0 <environment> <server> [instance id]"
+    echo "usage: $0 <environment> <asg> <server> [instance IP|scp]"
     echo
     echo "Environments:"
     for ENVIRONMENT in "${ENVIRONMENT_LIST[@]}"
@@ -17,10 +77,12 @@ function usage()
 	echo -e "\t${ENVIRONMENT}"
     done
     echo
-    echo -e "Servers:"    
-    for SERVER in "${SERVER_LIST[@]}"
+    echo -e "Servers:"
+    for ASG_GRP in "${ASG_LIST[@]}"
     do
-	echo -e "\t${SERVER}"
+	ASG_NAME=$(echo $ASG_GRP | cut -d ':' -f 1)
+	SERVER_NAME=$(echo $ASG_GRP | cut -d ':' -f 2)	
+	echo -e "\t${ASG_NAME} ${SERVER_NAME}"
     done
     
     exit 1
@@ -38,51 +100,61 @@ function declare_environment()
     ENVIRONMENT_LIST+=("$ENVIRONMENT")
 }
 
-function declare_server()
+function declare_asg()
 {
-    SERVER="$1"
+    GROUP="$1"
+    ASG="$2"
 
-    SERVER_LIST+=("$SERVER")
+    ASG_LIST+=("$GROUP:$ASG")
 }
 
 function process_args()
 {
-
+    
     # Assert: Environment exists
     if [[ ! " ${ENVIRONMENT_LIST[*]} " =~ " ${ENVIRONMENT_ARG} " ]]; then
 	usage
     fi
     
-    if [[ ! " ${SERVER_LIST[*]} " =~ " ${SERVER_ARG} " ]]; then
+    # Assert: ASG exists
+    FOUND=0
+    for ASG_GRP in "${ASG_LIST[@]}"
+    do
+	ASG_NAME=$(echo $ASG_GRP | cut -d ':' -f 1)
+	SERVER_NAME=$(echo $ASG_GRP | cut -d ':' -f 2)
+	if [ "$ASG_NAME" == "$ASG_ARG" -a "$SERVER_NAME" == "$SERVER_ARG" ] ; then
+	    FOUND=1
+	    break
+	fi
+    done
+    if [ $FOUND -eq 0 ] ; then
+	echo "ERROR: Unable to find ASG and Server: $ASG_ARG $SERVER_ARG"
 	usage
-    fi   
+    fi
     
     # Map the Alias environment
+    PROFILE_ENVIRONMENT="$ENVIRONMENT_ARG"
     for ALIAS_KVP in "${ENVIRONMENT_ALIAS[@]}"
     do
 	ALIAS=$(echo $ALIAS_KVP | cut -d '=' -f 1)
 	if [ "$ALIAS" == "$ENVIRONMENT_ARG" ] ; then
-	    ENVIRONMENT_ARG=$(echo $ALIAS_KVP | cut -d '=' -f 2)
-	    SUB_ENVIRONMENT="$ENVIRONMENT-"
+	    ENVIRONMENT_ARG="$ENVIRONMENT"
+	    PROFILE_ENVIRONMENT=$(echo $ALIAS_KVP | cut -d '=' -f 2)
 	    break
 	fi
     done
 }
 
+
 function get_unique_port()
 {
     ENVIRONMENT=$1
-    SERVER=$2
+    ASG=$2
     
     # User Ports Range
     PORT_START=40000
     PORT_END=49151
 
-    CALLER=$(basename $(caller | awk '{print $2}'))
-
-    PORT_FILE_FOLDER=~/.paco-ssh
-    PORT_FILE="${PORT_FILE_FOLDER}/ssh-local-ports"
-    PORT_CACHE_FILE="${PORT_FILE_FOLDER}/${PROJECT_NAME}-${ENVIRONMENT}-${SERVER}.cache"
     if [ ! -e "$PORT_FILE" ] ; then
 	mkdir -p $PORT_FILE_FOLDER
 	:> $PORT_FILE
@@ -111,46 +183,65 @@ function get_unique_port()
     done
 }
 
-function ssh_command()
+function ssm_command()
 {
+    COMMAND_ARG=$1
+    shift
+
+    if [ "$COMMAND_ARG" == "" ] ; then
+	COMMAND_ARG="ssh"
+    fi
+
     # Process Command Arguments
     process_args
-    # Generate a local port to use    
-    LOCAL_PORT=$(get_unique_port $ENVIRONMENT_ARG $SERVER_ARG)
-        
-    ENVIRONMENT_U=$(tr '[:lower:]' '[:upper:]' <<< ${ENVIRONMENT_ARG:0:1})${ENVIRONMENT_ARG:1}
-    SERVER_U=$(tr '[:lower:]' '[:upper:]' <<< ${SERVER_ARG:0:1})${SERVER_ARG:1}
-    PROJECT_U=$(tr '[:lower:]' '[:upper:]' <<< ${PROJECT_NAME:0:1})${PROJECT_NAME:1}
+    # Generate a local port to use
+    LOCAL_PORT=$(get_unique_port $ENVIRONMENT_ARG $ASG_ARG)
+
+    if [ "$SUB_ENVIRONMENT" == "" ] ; then
+	ENVIRONMENT_U=$(tr '[:lower:]' '[:upper:]' <<< ${ENVIRONMENT_ARG:0:1})${ENVIRONMENT_ARG:1}
+    else
+	ENVIRONMENT_U=$(tr '[:lower:]' '[:upper:]' <<< ${SUB_ENVIRONMENT:0:1})${SUB_ENVIRONMENT:1}
+    fi
+    ASG_ARG_U=$(tr '[:lower:]' '[:upper:]' <<< ${ASG_ARG:0:1})${ASG_ARG:1}
+    SERVER_ARG_U=$(tr '[:lower:]' '[:upper:]' <<< ${SERVER_ARG:0:1})${SERVER_ARG:1}
+    PROJECT_U=$(tr '[:lower:]' '[:upper:]' <<< ${NETENV_NAME:0:1})${NETENV_NAME:1}
     APPLICATION_U=$(tr '[:lower:]' '[:upper:]' <<< ${APPLICATION_NAME:0:1})${APPLICATION_NAME:1}
     
     # Upper case ENVIRONMENT
-    SERVER_NAME="${PROJECT_U}-${ENVIRONMENT_U}-${APPLICATION_U}-${SUB_ENVIRONMENT}${SERVER_U}-Server"
-    AWS_PROFILE="${PROJECT_NAME}-$ENVIRONMENT_ARG"
+    #ASG_NAME="${PROJECT_U}-${ENVIRONMENT_U}-${APPLICATION_U}-${SUB_ENVIRONMENT}${ASG_ARG_U}-${SERVER_ARG_U}"
+    ASG_NAME="${PROJECT_U}-${ENVIRONMENT_U}-${APPLICATION_U}-${ASG_ARG_U}-${SERVER_ARG_U}"
+    AWS_PROFILE="${NETENV_NAME}-${PROFILE_ENVIRONMENT}"
     
-    if [ "$INSTANCE_ID_ARG" != "" ] ; then
-	SERVER_NAME="$INSTANCE_ID_ARG"	
+    if [ "$INSTANCE_IP_ARG" != "" ] ; then
+	ASG_NAME="$INSTANCE_IP_ARG"	
     fi
-    echo ssm_ssh $AWS_PROFILE $SERVER_NAME $SSH_USERNAME $SSH_PRIVATE_KEY $LOCAL_PORT
-    ssm_ssh $AWS_PROFILE $SERVER_NAME $SSH_USERNAME $SSH_PRIVATE_KEY $LOCAL_PORT    
+    echo "COMMAND_ARG: $COMMAND_ARG"
+    COMMAND=$COMMAND_ARG
+    case $COMMAND_ARG in
+	"scp") 
+	    COMMAND_SOURCE=$1
+	    shift
+	    COMMAND_DEST=$1
+	    shift
+	    COMMAND=ssh
+	    ;;	
+    esac    
+
+    INSTANCE_IP=$1
+    shift
+    case $COMMAND in
+	"ssh")
+	    echo ssm_ssh $AWS_PROFILE $ASG_NAME $SSH_USERNAME $SSH_PRIVATE_KEY $LOCAL_PORT $COMMAND_ARG $COMMAND_SOURCE $COMMAND_DEST
+	    ssm_ssh $AWS_PROFILE $ASG_NAME $SSH_USERNAME $SSH_PRIVATE_KEY $LOCAL_PORT $COMMAND_ARG $COMMAND_SOURCE $COMMAND_DEST
+	    ;;
+	"port_forward")
+	    REMOTE_PORT=$2
+	    echo ssm_port_forward $AWS_PROFILE $ASG_NAME $LOCAL_PORT $REMOTE_PORT $INSTANCE_IP
+	    ssm_port_forward $AWS_PROFILE $ASG_NAME $LOCAL_PORT $REMOTE_PORT $INSTANCE_IP
+	    ;;	    
+    esac
 }
 
-
-function valid_ip()
-{
-    local  ip=$1
-    local  stat=1
-
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        OIFS=$IFS
-        IFS='.'
-        ip=($ip)
-        IFS=$OIFS
-        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
-            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-        stat=$?
-    fi
-    return $stat
-}
 
 function cache_mfa()
 {
@@ -162,23 +253,22 @@ function cache_mfa()
 function get_target_instance_id()
 {
     AWS_PROFILE=$1
-    SERVER_NAME=$2
+    ASG_NAME=$2
 
-    FILTER=Name=tag:Name,Values=$SERVER_NAME    
-    if valid_ip $SERVER_NAME; then 
-	FILTER="Name=network-interface.addresses.private-ip-address,Values=$SERVER_NAME"
+    FILTER=Name=tag:Name,Values=$ASG_NAME    
+    if valid_ip $ASG_NAME; then
+	FILTER="Name=network-interface.addresses.private-ip-address,Values=$ASG_NAME"
     fi
 
-    echo aws ec2 describe-instances --profile $AWS_PROFILE --query "Reservations[0].Instances[*].[InstanceId]" --filters $FILTER Name=instance-state-name,Values=running --output text  >/tmp/aws.out
     aws ec2 describe-instances --profile $AWS_PROFILE --query "Reservations[0].Instances[*].[InstanceId]" --filters $FILTER Name=instance-state-name,Values=running --output text 
 }
 
 function ssm_ssh_session()
 {
     AWS_PROFILE=$1
-    SERVER_NAME=$2    
+    ASG_NAME=$2    
 
-    INSTANCE_ID=$(get_target_instance_id $AWS_PROFILE $SERVER_NAME)
+    INSTANCE_ID=$(get_target_instance_id $AWS_PROFILE $ASG_NAME)
 
     aws ssm start-session --profile $AWS_PROFILE --target $INSTANCE_ID
 }
@@ -187,13 +277,13 @@ function ssm_port_forward()
 {
     #echo "Starting session for instance: $2 on port $1"
     AWS_PROFILE=$1
-    SERVER_NAME=$2
+    ASG_NAME=$2
     LOCAL_PORT=$3
     REMOTE_PORT=$4
 
-    INSTANCE_ID=$(get_target_instance_id $AWS_PROFILE $SERVER_NAME)
+    INSTANCE_ID=$(get_target_instance_id $AWS_PROFILE $ASG_NAME)
     if [ "$INSTANCE_ID" == "None" -o "$INSTANCE_ID" == "" ] ; then
-	echo "ERROR: Unable to get Instance ID for Server Name: $SERVER_NAME"
+	echo "ERROR: Unable to get Instance ID for Server Name: $ASG_NAME"
 	exit 1
     fi
 
@@ -206,7 +296,7 @@ function ssm_port_forward()
 function ssm_ssh()
 {
     AWS_PROFILE=$1
-    SERVER_NAME=$2    
+    ASG_NAME=$2    
     SSH_USER=$3
     SSH_PRIVATE_KEY=$4
     LOCAL_PORT=$5
@@ -220,7 +310,8 @@ function ssm_ssh()
 
     ps awux |grep session-manager-plugin |grep "localPortNumber\": \[\"${LOCAL_PORT}\"" >/dev/null 2>&1
     if [ $? -ne 0 ] ; then
-	ssm_port_forward $AWS_PROFILE $SERVER_NAME $LOCAL_PORT 22 >$SSM_LOG 2>&1 &
+	ssm_port_forward $AWS_PROFILE $ASG_NAME $LOCAL_PORT 22 >$SSM_LOG 2>&1 &
+	PID=$!
 	COUNT=0
 	TIMEOUT_SECS=20
 	while :
@@ -229,11 +320,14 @@ function ssm_ssh()
 	    if [ $? -eq 0 ] ; then
 		echo "ERROR: Session already started on localhost:$LOCAL_PORT"
 		cat $SSM_LOG
+		rm -f $PORT_CACHE_FILE
 		exit 1
 	    fi
 	    grep "Waiting for connections" $SSM_LOG >/dev/null 2>&1
 	    if [ $? -eq 0 ] ; then
 		echo "Opening SSH connection on new session: localhost:$LOCAL_PORT"
+		echo "$PID >$PORT_CACHE_FILE.smp-pid"
+		echo $PID >$PORT_PID_CACHE_FILE
 		break
 	    fi
 	    grep "An error occurred" $SSM_LOG >/dev/null 2>&1
@@ -245,6 +339,7 @@ function ssm_ssh()
 	    if [ $COUNT -eq $TIMEOUT_SECS ] ; then
 		echo "ERROR: Timedout waiting for ssm session:"
 		cat $SSM_LOG
+		rm -f $PORT_CACHE_FILE
 		exit 1
 	    fi
 	    COUNT=$(($COUNT + 1))
